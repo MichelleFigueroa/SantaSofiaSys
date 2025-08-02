a583771c7d12a195bab7bad30b9cdf02fe7e7a07
MichelleFigueroa <michellefigueroa335@outlook.com>
2025-08-01 19:51:11 -0600

    Creacuin de entidades Distrito y Usuario

---
+++ src/main/java/org/esfe/modelos/Distrito.java
@@ -0,0 +1,34 @@
+package org.esfe.modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class Distrito {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+
+         @NotBlank(message = "El nombre del distrito es obligatorio")
+          private String nombre;
+        @NotNull(message = "El municipio es obligatorio")
+
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+    }
+
---
+++ src/main/java/org/esfe/modelos/Usuario.java
@@ -0,0 +1,57 @@
+package org.esfe.modelos;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.math.BigInteger;
+
+public class Usuario {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private BigInteger id;
+
+        @NotBlank(message = "El rol es obligatorio")
+        private Integer idRol;
+
+        @NotBlank(message = "El empleado es obligatorio")
+        private BigInteger idEmpleado;
+
+        @NotBlank(message = "El nombre de usuario es obligatorio")
+        private String nombre;
+
+
+
+        public BigInteger getId() {
+            return id;
+        }
+
+        public void setId(BigInteger id) {
+            this.id = id;
+        }
+
+        public Integer getIdRol() {
+            return idRol;
+        }
+
+        public void setIdRol(Integer idRol) {
+            this.idRol = idRol;
+        }
+
+        public BigInteger getIdEmpleado() {
+            return idEmpleado;
+        }
+
+        public void setIdEmpleado(BigInteger idEmpleado) {
+            this.idEmpleado = idEmpleado;
+        }
+         public String getNombre() {
+        return nombre;
+         }
+
+        public void setNombre(String nombre) {
+        this.nombre = nombre;
+        }
+
+    }
+
