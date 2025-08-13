a3578d729106a06ec614e8bbc3e671078681e6cb
BrandonEscobar21 <brandonparedesacb2020@gmail.com>
2025-08-13 14:29:52 -0600

    Controlador de Empresa creado.

---
+++ src/main/java/org/esfe/controladores/EmpresaController.java
@@ -0,0 +1,45 @@
+package org.esfe.controladores;
+
+import org.esfe.modelos.Empresa;
+import org.esfe.servicios.interfaces.IEmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Controller
+@RequestMapping("/empresas")
+public class EmpresaController {
+    @Autowired
+    private IEmpresaService empresaService;
+
+
+    public String index(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size){
+        int currentPage = page.orElse(1) - 1; // Si no está seteado, se asigna 0
+        int pageSize = size.orElse(5); // Tamaño de la página, se asigna 5
+        Pageable pageable = PageRequest.of(currentPage, pageSize);
+
+        Page<Empresa> empresas = empresaService.buscarTodosPaginados(pageable);
+        model.addAttribute("empresas", empresas);
+
+        int totalPages = empresas.getTotalPages();
+        if (totalPages > 0) {
+            List<Integer> pageNumbers = IntStream.rangeClosed(1, totalPages)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
+        return "empresa/index";
+    }
+}
