4a0ba58812627a78c0b6d63457a65a49b31bd52e
M61510 <CL24004@esfe.agape.edu.sv>
2025-08-14 17:11:06 -0600

    index productocontrolle actualizado en  sus cambios

--- src/main/java/org/esfe/controladores/ProductoController.java
+++ src/main/java/org/esfe/controladores/ProductoController.java
@@ -8,9 +8,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -43,5 +43,27 @@ public class ProductoController {
         }
 
         return "producto/index";
+    }
+    @GetMapping("/create")
+    public String create (Producto producto){
+        return "producto/create";
+    }
+
+    @PostMapping("/save")
+    public String save (Producto producto, BindingResult result, Model model, RedirectAttributes attributes){
+        if(result.hasErrors()){
+            model.addAttribute(producto);
+            attributes.addFlashAttribute("error", "No se pudo guardar debido aun error.");
+            return "producto/create";
+        }
+        productoService.crearOEditar(producto);
+        attributes.addFlashAttribute("msg", "producto creado correctamente");
+        return "redirect:/producto";
+    }
+    @GetMapping("/details/{id}")
+    public String details (@PathVariable("id") Integer id, Model model){
+        Producto producto = productoService.buscarPorId(id).get();
+        model.addAttribute("producto", producto);
+        return "producto/details";
     }
 }
